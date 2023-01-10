d172326155d59372eecd4539571b82dfc4227fa4
Artur <arturvpc@gmail.com>
2023-01-10 10:17:13 -0300

    Codigo

---
+++ app.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+  // Inicializa o navegador
+  const browser = await puppeteer.launch();
+
+  // Abre uma nova página
+  const page = await browser.newPage();
+
+  // Vai para o site do Google
+  await page.goto('https://www.google.com/');
+
+  // Digita a palavra "teste" na barra de pesquisa
+  await page.type('input[name=q]', 'teste');
+
+  // Envia o formulário de pesquisa
+  await page.evaluate(() => {
+    document.querySelector('form[name=f]').submit();
+  });
+
+  // Espera até que a página carregue os resultados da pesquisa
+  await page.waitForSelector('#search');
+
+  // Fecha o navegador
+  await browser.close();
+}
+
+run();
